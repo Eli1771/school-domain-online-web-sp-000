ff916df34f46d1dc1a6395b166e312c28a7dfb57
elijahmm17 <elijahmm17@gmail.com>
2019-05-19 22:21:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,6 +9,11 @@ class School
   end 
   
   def add_student(name, grade)
-    roster.keys.include?(grade) ? @roster[grade] << name : @roster[grade] = [] @roster[grade] << name 
+    if !roster.keys.include?(grade)
+      @roster[grade] = []
+      @roster[grade] << name
+    else 
+      @roster[grade] << name
+    end
   end
 end 
